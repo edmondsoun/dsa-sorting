53672e499635ae4c8633df124853d59cbf678529
Edmond Soun <edmond.soun@gmail.com>
2022-09-23 11:26:04 -0700

    solution for bubblesort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,20 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+    //[5,3,2,1,4]
+
+    //two loops
+
+    // Loop with i from end of array towards beginning
+    for (let i = array.length; i >= 0; i--) {
+        // Loop with j from the beginning until i - 1
+        for (let j = 0; j < i-1; j++) {
+            if (array[j] > array[j + 1]) {
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            }
+        }
+    }
+
+    return array;
+
+}
 
 module.exports = bubbleSort;
